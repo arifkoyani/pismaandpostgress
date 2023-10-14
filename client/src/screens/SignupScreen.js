ce291290eedf62fc64f25c0146761186b531c0cf
Aesha58 <shabbiralam45524@gmail.com>
2023-10-14 23:31:27 +0500

    off

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -45,7 +45,7 @@ const RegisterPage = ({navigation}) => {
         };
     
         try{
-          const response = await axios.post("http://192.168.18.30:5000/api/users/register", customerData)
+          const response = await axios.post("https://off-api.vercel.app/api/users/register", customerData)
            console.log(response)
            navigation.navigate("Signin")
         }catch(error){
