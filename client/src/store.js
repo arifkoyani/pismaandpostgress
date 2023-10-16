6e0b7ef36c2f70b88df0bd0d6e51820c43a8ce57
shabbir <shabbiralam45524@gmail.com>
2023-10-16 23:17:07 +0500

    h

--- client/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./features/BasketSlice";
-import userReducer from "./features/UserSlice";
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
-});
