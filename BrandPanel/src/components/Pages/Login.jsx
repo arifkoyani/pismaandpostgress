e1406e3866262676953973b0155a3a52e109e06f
Shabbir <shabbiralam45524@gmail.com>
2023-10-28 18:25:51 +0500

    Update Login.jsx

--- BrandPanel/src/components/Pages/Login.jsx
+++ BrandPanel/src/components/Pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
   return  (
     <div className="login-container">
      <div className="login-form">
-      <h2 className='heading'>Admin Login</h2>
+      <h2 className='heading'>Brand Login</h2>
       <form>
         <div className="form-group ">
           <label htmlFor="email">Email:</label>
